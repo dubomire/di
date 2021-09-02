09dac900f99a096229d6b1fcd6e00cff2add71fa
dubomire <dubomire@gmail.com>
2021-09-02 10:46:09 +0300

    Injecting MainForm. Parameterless ctor removed

--- FractalPainter/App/Program.cs
+++ FractalPainter/App/Program.cs
@@ -1,32 +1,47 @@
 ﻿using System;
 using System.Windows.Forms;
+using FractalPainting.App.Actions;
+using FractalPainting.Infrastructure.UiActions;
 using Ninject;
 
 namespace FractalPainting.App
 {
     internal static class Program
     {
-        /// <summary>
-        ///     The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        private static void Main()
+        private static void RunApp(IKernel container)
         {
             try
             {
-                var container = new StandardKernel();
-
-                // start here
-                // container.Bind<TService>().To<TImplementation>();
-
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new MainForm());
+                Application.Run(container.Get<MainForm>());
             }
             catch (Exception e)
             {
                 MessageBox.Show(e.Message);
             }
         }
+
+        private static void ConfigureInjections(IKernel container)
+        {
+            container.Bind<IUiAction>().To<SaveImageAction>();
+            container.Bind<IUiAction>().To<DragonFractalAction>();
+            container.Bind<IUiAction>().To<KochFractalAction>();
+            container.Bind<IUiAction>().To<ImageSettingsAction>();
+            container.Bind<IUiAction>().To<PaletteSettingsAction>();
+        }
+        
+        /// <summary>
+        ///     The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            IKernel container = new StandardKernel();
+            ConfigureInjections(container);
+
+            RunApp(container);
+        }
+
     }
 }
