8475a3a9d7718ac97948e359df3064799ea95b8b
dubomire <dubomire@gmail.com>
2021-09-02 13:25:32 +0300

    Removed redundant factory interface

--- FractalPainter/App/Fractals/IDragonPainterFactory.cs
+++
@@ -1,9 +0,0 @@
-using FractalPainting.Infrastructure.Common;
-
-namespace FractalPainting.App.Fractals
-{
-    public interface IDragonPainterFactory
-    {
-        DragonPainter CreateDragonPainter(DragonSettings settings);
-    }
-}
